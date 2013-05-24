14bec2f3457f982df6625419eae0fadba6f44548
Joan <joan@joan-VPCEB1M1E.(none)>
2013-05-24 22:21:17 +0200

    Binaris

--- Bluetooth_Android/Projecte/RobotsPEC/gen/com/example/robotspec/R.java
+++ Bluetooth_Android/Projecte/RobotsPEC/gen/com/example/robotspec/R.java
@@ -24,24 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000d;
-        public static final int btnEnvia=0x7f080008;
-        public static final int btnMaster=0x7f080009;
-        public static final int btnSlave=0x7f080007;
-        public static final int button1=0x7f080006;
-        public static final int button2=0x7f080005;
-        public static final int quefaig=0x7f08000a;
-        public static final int quisoc=0x7f08000b;
-        public static final int tableRow1=0x7f080001;
-        public static final int tableRow2=0x7f080002;
-        public static final int tableRow3=0x7f080003;
-        public static final int tableRow4=0x7f080004;
+        public static final int action_settings=0x7f080005;
+        public static final int btnEnvia=0x7f080002;
+        public static final int button1=0x7f080001;
+        public static final int button2=0x7f080004;
+        public static final int tableLayout1=0x7f080003;
         public static final int textView1=0x7f080000;
-        public static final int txtrebut=0x7f08000c;
     }
     public static final class layout {
         public static final int activity_autobot=0x7f030000;
-        public static final int activity_deception=0x7f030001;
+        public static final int activity_decepticon=0x7f030001;
         public static final int activity_main=0x7f030002;
     }
     public static final class menu {
@@ -51,10 +43,11 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int autobot_tag=0x7f050004;
-        public static final int deception_tag=0x7f050005;
+        public static final int decepticon_tag=0x7f050005;
         public static final int enrere_tag=0x7f050006;
         public static final int escull_tag=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int shoot=0x7f050007;
     }
     public static final class style {
         /** 
